84a640ebcdd25e99567e778e8688be80aa3ecc10
Eniamza <eniamza0@gmail.com>
2024-01-07 02:27:45 +0600

    Added initial Socketio implementation

--- src/auction/livebiddingSinglePlayer.js
+++ src/auction/livebiddingSinglePlayer.js
@@ -1,12 +1,23 @@
+const { getFirestore } = require('firebase-admin/firestore');
+let db = getFirestore()
+
+let teamRef = db.collection('teams');
+let playerRef = db.collection('players');
+let auctionRef = db.collection('auctions');
+
+const {getSingleAuction,getSinglePlayer,getAllTeams, getSinglePlayerByID} = require("../firebase/utils/firestore")
+
 const fs = require('fs').promises;
 
 let currentBidPlayer = {}
+let currentAuction
 let currentHighestBid = 0
 let currentHighestBidder = ""
 let currentBidStatus = ""    // "open" or "closed"
 let teamBalances = {} // {teamId: balance}
 let teamLoans = {} // {teamId: loanedAmount} Loaned amount must be less than the team Balance
 let bidQueue = []
+let aucHistory = [] // {playerId: {teamId: bidAmount}} // This is the history of the auction called on bidEnded
 
 
 
@@ -14,20 +25,100 @@ let bidQueue = []
     // and then an updateBid is called, this script should update the variables with the new bid
     // Write the neccesary variables
 
-let startBidPlayer = async function (player) {
+let checkCurrentBid = async function (bidder, bid) {
+
+    if (currentBidStatus === "closed") {
+        return false
+    }
+
+    if (bid <= currentHighestBid) {
+        return false
+    }
+
+    if (bid > teamBalances[bidder]) {
+        return false
+    }
+
+    if (bid > teamBalances[bidder] + teamLoans[bidder]) {
+        return false
+    }
+
+    return true
+
+
+}
+
+let replytoBidStart = async function () {
+
+    try {
+
+        let message = {
+            currentBidStatus,
+            currentBidPlayer,
+            currentHighestBid,
+            currentHighestBidder,
+            currentAuction
+    
+        }
+    
+        return message
+        
+    } catch (error) {
+
+        console.log(error)
+        return false
+        
+    }
+
+}
+
+let startBidPlayer = async function (playerid,auctionid) {
     try {
 
         if (currentBidStatus === "open") {
+            console.log("Bid is already running")
             return false
         }
 
+        if(playerid === undefined || auctionid === undefined){
+            console.log("Player or Auction ID is undefined")
+            return false
+        }
+
+        let player = await getSinglePlayerByID(playerid)
+        let auction = await getSingleAuction(auctionid)
+
+        if(player === null || auction === null){
+
+            console.log("Player or Auction is null. Please provide proper IDs")
+            return false
+        }
+
+        let allTeams = await getAllTeams() 
+        allTeams = allTeams.teams
+
+        allTeams.forEach(team => {
+
+            teamBalances[team.id] = team.balance
+
+
+        });
+
+        console.log(teamBalances)
+
         currentBidPlayer = player
+        currentAuction = auction
         currentHighestBid = 0
         currentHighestBidder = ""
         currentBidStatus = "open"
+        teamLoans = {}
+        bidQueue = []
+
         await saveData();
-        return true
+        return await replytoBidStart()
+
     } catch (error) {
+        console.log(error)
         return false
     }
 }
@@ -57,7 +148,12 @@ let saveData = async function() {
     const data = {
         currentBidPlayer,
         currentHighestBid,
-        currentHighestBidder
+        currentHighestBidder,
+        currentBidStatus,
+        teamLoans,
+        bidQueue,
+        aucHistory,
+        currentAuction
     };
     await fs.writeFile('data.json', JSON.stringify(data));
 }
@@ -68,6 +164,12 @@ let loadData = async function() {
         currentBidPlayer = data.currentBidPlayer;
         currentHighestBid = data.currentHighestBid;
         currentHighestBidder = data.currentHighestBidder;
+        currentBidStatus = data.currentBidStatus;
+        teamLoans = data.teamLoans;
+        bidQueue = data.bidQueue;
+        aucHistory = data.aucHistory;
+        currentAuction = data.currentAuction;
+
     } catch (error) {
         console.error('Failed to load data:', error);
     }
@@ -75,3 +177,8 @@ let loadData = async function() {
 
 // Load the data when the module is loaded
 loadData();
+
+module.exports = {
+    startBidPlayer,
+    replytoBidStart
+}
--- src/socketio/admin.js
+++ src/socketio/admin.js
@@ -1,5 +1,7 @@
 const {admin} = require("../firebase/index")
 
+const {startBidPlayer,replytoBidStart} = require("../auction/livebiddingSinglePlayer")
+
 module.exports = async function (io,socket) {
 
     decoded = await admin.auth().verifyIdToken(socket.handshake.headers.idtoken,true)
@@ -8,8 +10,36 @@ module.exports = async function (io,socket) {
     console.log(socket.uid)
     console.log("New Admin connected:",socket.id);
 
-    socket.on("bidStart",()=>{
-        io.emit("bidStart","BID HAS STARTEDDDDD")
+    socket.on("bidStart", async (message)=>{
+
+        try {
+
+            if(!message){
+                return socket.emit("error","Please pass the playerID and auctionID")
+            }
+    
+            message = JSON.parse(message)
+    
+            let playerID = message.playerID
+            let auctionID = message.auctionID
+            let resBidStart = await startBidPlayer(playerID,auctionID)
+            console.log(resBidStart)
+    
+            if(!resBidStart){
+                socket.emit("error","Oops, Either the bid is running or an error has occured, Check the logs!")
+                socket.emit("currentBid",await replytoBidStart())
+                return
+            }
+    
+            io.emit("bidStarted",resBidStart)
+            
+        } catch (error) {
+
+            console.log(error)
+            return socket.emit("error", "The request was malformed, Please check the logs for more info")
+            
+        }
+
     })
 
 
